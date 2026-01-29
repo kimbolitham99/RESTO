3fe32da780af80a749985ee4e55e6751eaf7ebe6
kimbolitham99 <kimbolitham99@gmail.com>
2026-01-29 14:35:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,19 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  // Tambahkan baris base di bawah ini agar file CSS/JS terbaca di GitHub Pages
   base: "/RESTO/", 
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()], // viteSingleFile dihapus
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    // Memastikan hasil build masuk ke folder 'dist' yang dikenali GitHub Actions
     outDir: "dist",
   },
 });
