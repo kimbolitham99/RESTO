66049184bbce5d3d994d49c4118656b1410f0925
kimbolitham99 <kimbolitham99@gmail.com>
2026-01-29 14:18:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,16 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Tambahkan baris base di bawah ini agar file CSS/JS terbaca di GitHub Pages
+  base: "/RESTO/", 
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    // Memastikan hasil build masuk ke folder 'dist' yang dikenali GitHub Actions
+    outDir: "dist",
   },
 });
